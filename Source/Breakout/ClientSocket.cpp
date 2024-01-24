498d15a211814ff49b2ab7eb11ef280e37cd5816
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-01-24 21:03:50 +0900

    create clientsocket

---
+++ Source/Breakout/ClientSocket.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClientSocket.h"
+#include <sstream>
+#include <process.h>
+#include "Game/BOGameMode.h"
+#include "Runtime/Core/Public/GenericPlatform/GenericPlatformAffinity.h"
+#include "Runtime/Core/Public/HAL/RunnableThread.h"
+
+ClientSocket::ClientSocket()
+	:StopTaskCounter(0)
+{}
+
+ClientSocket::~ClientSocket() {
+	delete Thread;
+	Thread = nullptr;
+
+	closesocket(ServerSocket);
+	WSACleanup();
+}
+
+bool ClientSocket::InitSocket()
+{
+	WSADATA wsaData;
+	// 윈속 버전을 2.2로 초기화
+	int nResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (nResult != 0) {
+		UE_LOG(LogTemp, Error, TEXT("INITAILAIZING ERROR"));
+		return false;
+	}
+
+	// TCP 소켓 생성
+	ServerSocket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_IP, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (ServerSocket == INVALID_SOCKET) {
+		return false;
+	}
+	return true;
+}
+
+bool ClientSocket::Connect(const char* s_IP, int port)
+{
+	// 접속할 서버 정보를 저장할 구조체
+	SOCKADDR_IN stServerAddr;
+	// 접속할 서버 포트 및 IP
+	stServerAddr.sin_family = AF_INET;
+	stServerAddr.sin_port = htons(port);
+	stServerAddr.sin_addr.s_addr = inet_addr(s_IP);
+
+	int nResult = connect(ServerSocket, (sockaddr*)&stServerAddr, sizeof(sockaddr));
+	if (nResult == SOCKET_ERROR) {
+		return false;
+	}
+	return true;
+}
+
+void ClientSocket::SetGameMode(ABOGameMode* pGameMode)
+{
+	GameMode = pGameMode;
+}
+
+void ClientSocket::CloseSocket()
+{
+	closesocket(ServerSocket);
+	WSACleanup();
+}
+
+bool ClientSocket::Init()
+{
+	return true;
+}
+uint32 ClientSocket::Run()
+{
+	FPlatformProcess::Sleep(0.03);
+	// 게임모드를 가져옴
+	ABOGameMode* LocalGameMode = nullptr;
+	if (GameMode != nullptr) {
+		LocalGameMode = GameMode;
+	}
+	return 0;
+}
+void ClientSocket::Stop()
+{
+	// thread safety 변수를 조작해 while loop 가 돌지 못하게 함
+	StopTaskCounter.Increment();
+}
+
+void ClientSocket::Exit()
+{
+}
+
+bool ClientSocket::StartListen()
+{
+	// 스레드 시작
+	if (Thread != nullptr) return false;
+	Thread = FRunnableThread::Create(this, TEXT("ClientSocket"), 0, TPri_BelowNormal);
+	return (Thread != nullptr);
+}
+
+void ClientSocket::StopListen()
+{
+	// 스레드 종료
+	Stop();
+	Thread->WaitForCompletion();
+	Thread->Kill();
+	delete Thread;
+	Thread = nullptr;
+	StopTaskCounter.Reset();
+}
---
+++ Source/Breakout/ClientSocket.h
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Windows/AllowWindowsPlatformTypes.h"
+#include "Windows/PreWindowsApi.h"
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <fstream>
+#include <iostream>
+#include "Windows/PostWindowsApi.h"
+#include "Windows/HideWindowsPlatformTypes.h"
+#include "CoreMinimal.h"
+
+
+#define	MAX_BUFFER		4096
+#define SERVER_PORT		12345
+#define SERVER_IP		"127.0.0.1"
+#define MAX_CLIENTS		100
+
+class ABOGameMode;
+class ACharacterController;
+using namespace std;
+
+// 플레이어 클래스 
+class Player
+{
+public:
+	Player() { };
+	~Player() {};
+
+	// 세션 아이디
+	int Id = -1;
+	// 아이디 비번
+	char	userId[20] = {};
+	char	userPw[20] = {};
+	// 위치
+	float X = 0;
+	float Y = 0;
+	float Z = 0;
+	// 회전값
+	float Yaw = 0;
+	float Pitch = 0;
+	float Roll = 0;
+	// 속도
+	float VeloX = 0;
+	float VeloY = 0;
+	float VeloZ = 0;
+
+	friend ostream& operator<<(ostream& stream, Player& info)
+	{
+		stream << info.Id << endl;
+		stream << info.X << endl;
+		stream << info.Y << endl;
+		stream << info.Z << endl;
+		stream << info.VeloX << endl;
+		stream << info.VeloY << endl;
+		stream << info.VeloZ << endl;
+		stream << info.Yaw << endl;
+		stream << info.Pitch << endl;
+		stream << info.Roll << endl;
+		return stream;
+	}
+
+	friend istream& operator>>(istream& stream, Player& info)
+	{
+		stream >> info.Id;
+		stream >> info.X;
+		stream >> info.Y;
+		stream >> info.Z;
+		stream >> info.VeloX;
+		stream >> info.VeloY;
+		stream >> info.VeloZ;
+		stream >> info.Yaw;
+		stream >> info.Pitch;
+		stream >> info.Roll;
+		return stream;
+	}
+};
+/**
+ *
+ */
+class BREAKOUT_API ClientSocket : public FRunnable
+{
+public:
+	ClientSocket();
+	virtual ~ClientSocket();
+	bool InitSocket();
+	bool Connect(const char* s_IP, int port);
+	// 소켓이 속한 게임모드를 세팅해주는 함수
+	void SetGameMode(ABOGameMode* pGameMode);
+
+	void CloseSocket();
+
+	FRunnableThread* Thread;
+	FThreadSafeCounter StopTaskCounter;
+	virtual bool Init();
+	virtual uint32 Run();
+	virtual void Stop();
+	virtual void Exit();
+
+	// 스레드 시작 및 종료
+	bool StartListen();
+	void StopListen();
+
+	// 싱글턴 객체 가져오기
+	static ClientSocket* GetSingleton() {
+		static ClientSocket ins;
+		return &ins;
+	}
+private:
+	SOCKET ServerSocket;
+	char recvBuffer[MAX_BUFFER];
+	ABOGameMode* GameMode;	// 게임모드 정보
+};
