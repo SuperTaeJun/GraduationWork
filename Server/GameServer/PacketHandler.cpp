9d0b7dcb93cd5f1a4e1757568a26fa79a0c23fab
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-02-25 23:23:03 +0900

    Packetmanager 클래스 주석 해제
    여기서 패킷을 관리하기 위함

--- Server/GameServer/PacketHandler.cpp
+++ Server/GameServer/PacketHandler.cpp
@@ -1,39 +1,39 @@
 #include "pch.h"
 #include "PacketHandler.h"
-//array <ClientInfo, 100> clients;
-//void Login_Back(const CS_LOGIN_PACKET* packet, Overlapped* overlapped);
+array <clientinfo, 100> clients;
+void login_back(const cs_login_packet* packet, overlapped* overlapped);
 
-//void PacketHandler::ProcessPacket(int id, unsigned char* r_ptr)
-//{
-//	cout << "d";
-//	Idnum = id;
-//	recv_buf = r_ptr;
-//	cl = clients[Idnum];
-//	unsigned char packet_type = r_ptr[1];
-//
-//	switch (packet_type)
-//	{
-//	case CS_LOGIN:
-//		Login();
-//		break;
-//	default:
-//		break;
-//	}
-//}
+void packethandler::processpacket(int id, unsigned char* r_ptr)
+{
+	cout << "d";
+	idnum = id;
+	recv_buf = r_ptr;
+	cl = clients[idnum];
+	unsigned char packet_type = r_ptr[1];
 
-//
-//bool PacketHandler::Login()
-//{
-//	CS_LOGIN_PACKET* packet = reinterpret_cast<CS_LOGIN_PACKET*>(recv_buf);
-//	cout << "recv id : " << packet->id << "," << "recv pw : " << packet->pw << endl;
-//	Login_Back(Idnum);
-//	return true;
-//}
-//
-//int PacketHandler::get_id()
-//{
-//	static int g_id = 0;
-//	return g_id;
-//	
-//}
+	switch (packet_type)
+	{
+	case cs_login:
+		login();
+		break;
+	default:
+		break;
+	}
+}
+
+
+bool packethandler::login()
+{
+	cs_login_packet* packet = reinterpret_cast<cs_login_packet*>(recv_buf);
+	cout << "recv id : " << packet->id << "," << "recv pw : " << packet->pw << endl;
+	login_back(idnum);
+	return true;
+}
+
+int packethandler::get_id()
+{
+	static int g_id = 0;
+	return g_id;
+	
+}
 
--- Server/GameServer/PacketHandler.h
+++ Server/GameServer/PacketHandler.h
@@ -5,20 +5,20 @@
 #include "ClientInfo.h"
 #include "lOCPServer.h"
 
-//extern array <ClientInfo, 100> clients;
-//class PacketHandler
-//{
-//public:
-//	PacketHandler() {};
-//	virtual ~PacketHandler() {};
-//
-//public:
-//	//void ProcessPacket(int id, unsigned char* r_ptr);
-//	bool Login();
-//	int get_id();
-//private:
-//	ClientInfo* cl = nullptr;
-//	int Idnum = 0;
-//	unsigned char* recv_buf = nullptr;
-//};
-//
+extern array <ClientInfo, 100> clients;
+class PacketHandler
+{
+public:
+	PacketHandler() {};
+	virtual ~PacketHandler() {};
+
+public:
+	//void ProcessPacket(int id, unsigned char* r_ptr);
+	bool Login();
+	int get_id();
+private:
+	ClientInfo* cl = nullptr;
+	int Idnum = 0;
+	unsigned char* recv_buf = nullptr;
+};
+
