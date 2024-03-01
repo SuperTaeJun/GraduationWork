b9154098d4c7233574e953d2ce1f43a48108bf5e
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-03-02 01:00:32 +0900

    빌드 오류 문제 해결

--- Source/Breakout/Player/CharacterController.h
+++ Source/Breakout/Player/CharacterController.h
@@ -2,13 +2,14 @@
 
 
 #pragma once
-
+#include <queue>
 #include "CoreMinimal.h"
 #include "Kismet/GameplayStatics.h"
 #include "GameFramework/PlayerController.h"
 #include "CharacterController.generated.h"
+
 class ClientSocket;
-//class CPlayer;
+class CPlayer;
 class CPlayerInfo;
 /**
  * 
@@ -47,9 +48,9 @@ public:
 	void showWeaponSelect();
 	void RecvNewPlayer(int sessionID, float x, float y, float z);
 	void SendPlayerPos(int id);
-	//void SetNewCharacterInfo(shared_ptr<CPlayer> InitPlayer);
+	void SetNewCharacterInfo(std::shared_ptr<CPlayer*> InitPlayer);
 	//동기화 용
-	//void UpdateSyncPlayer();
+	void UpdateSyncPlayer();
 	// 스폰시킬 다른 캐릭터
 	UPROPERTY(EditAnywhere, Category = "Spawning")
 	TSubclassOf<class ACharacter> ToSpawn;
@@ -59,7 +60,7 @@ private:
 	bool bNewPlayerEntered = false;
 	ClientSocket* c_socket;
 	CPlayerInfo* PlayerInfo;  
-	//CPlayer initplayer;
+	CPlayer* initplayer;
 	// 다른 캐릭터들의 정보
-	//queue<shared_ptr<CPlayer>>	NewPlayer;
+	std::queue<CPlayer*> NewPlayer;
 };
