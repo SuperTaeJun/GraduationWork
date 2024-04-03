7392c8a1dfc4f97311f6bb287fd99dac01b31393
JHKimy <112988676+JHKimy@users.noreply.github.com>
2024-04-03 18:29:08 +0900

    폭탄 매쉬 추가

--- Server/Server/ServerCore/protocol.h
+++ Server/Server/ServerCore/protocol.h
@@ -1,7 +1,6 @@
 #pragma once
 #define SERVER_PORT		8001
-#define SERVER_IP		"192.168.219.103"
-
+#define SERVER_IP		"192.168.120.150"
 #define MAX_INFO_SIZE   20
 //const char CS_PACKET_ATTACK = 3;
 enum PlayerType
