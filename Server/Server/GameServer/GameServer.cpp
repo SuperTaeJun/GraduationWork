a06474c0504b09f90b077dbd131b74510b19dc49
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-04-14 23:46:01 +0900

    무기동기화 갑자기 안되었던 오류 해결

--- Server/Server/GameServer/GameServer.cpp
+++ Server/Server/GameServer/GameServer.cpp
@@ -367,24 +367,24 @@ void process_packet(int s_id, char* p)
 			cout << "이거 누구한테 감 :  ?" << other._s_id << endl; 
 			other.do_send(sizeof(packet), &packet);
 		}
-		for (auto& other : clients) {
-			if (other._s_id == cl._s_id) continue;
-			other.state_lock.lock();
-			if (ST_INGAME != other._state) {
-				other.state_lock.unlock();
-				continue;
-			}
-			else other.state_lock.unlock();
-			SC_SYNC_WEAPO packet;
-			packet.id = other._s_id;
-			packet.size = sizeof(packet);
-			packet.type = SC_OTHER_WEAPO;
-			packet.weapon_type = other.w_type;
-			packet.bselectwep = other.selectweapon;
-			//printf_s("[어떤 클라의 Send put object] id : %d, location : (%f,%f,%f), yaw : %f\n", packet.id, packet.x, packet.y, packet.z, packet.yaw);
-			cl.do_send(sizeof(packet), &packet);
-		}
-		break;
+		//for (auto& other : clients) {
+		//	if (other._s_id == cl._s_id) continue;
+		//	other.state_lock.lock();
+		//	if (ST_INGAME != other._state) {
+		//		other.state_lock.unlock();
+		//		continue;
+		//	}
+		//	else other.state_lock.unlock();
+		//	SC_SYNC_WEAPO packet;
+		//	packet.id = other._s_id;
+		//	packet.size = sizeof(packet);
+		//	packet.type = SC_OTHER_WEAPO;
+		//	packet.weapon_type = other.w_type;
+		//	packet.bselectwep = other.selectweapon;
+		//	//printf_s("[어떤 클라의 Send put object] id : %d, location : (%f,%f,%f), yaw : %f\n", packet.id, packet.x, packet.y, packet.z, packet.yaw);
+		//	cl.do_send(sizeof(packet), &packet);
+		//}
+		//break;
 	}
 	case CS_READY:
 	{
