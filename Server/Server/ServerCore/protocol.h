a4ef6c03f4fc96c70cce8ec209167f9cfa3845c1
SuperTaeJun <jtj990222@naver.com>
2024-04-16 13:26:07 +0900

    Update protocol.h

--- Server/Server/ServerCore/protocol.h
+++ Server/Server/ServerCore/protocol.h
@@ -2,7 +2,7 @@
 #define SERVER_PORT		8001
 #include <vector>
 #include <array>
-#define SERVER_IP		"192.168.219.101"
+#define SERVER_IP		"192.168.121.161"
 #define MAX_INFO_SIZE   20
 //const char CS_PACKET_ATTACK = 3;
 enum PlayerType
