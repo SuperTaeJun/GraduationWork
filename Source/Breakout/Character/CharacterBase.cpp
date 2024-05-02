e823a8e6ce211da2e3650b7a785cab946ffd212e
SuperTaeJun <jtj990222@naver.com>
2024-05-02 19:09:41 +0900

    Update CharacterBase.cpp

--- Source/Breakout/Character/CharacterBase.cpp
+++ Source/Breakout/Character/CharacterBase.cpp
@@ -438,7 +438,8 @@ void ACharacterBase::ReciveDamage(AActor* DamagedActor, float Damage, const UDam
 		ABOGameMode* GameMode = GetWorld()->GetAuthGameMode<ABOGameMode>();
 		if (GameMode)
 		{
-			GameMode->SetDamageInsigator(DamageInsigatorCh);
+			UE_LOG(LogTemp, Warning, TEXT("ID : %d"), DamageInsigatorCh->_SessionId);
+			//GameMode->SetDamageInsigator(DamageInsigatorCh);
 			//MainController = MainController == nullptr ? Cast<ACharacterController>(Controller) : MainController;
 			//ACharacterController* AttackerController = Cast<ACharacterController>(InstigatorController);
 			CurWeapon->CurAmmo = 0;
