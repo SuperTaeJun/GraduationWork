48aab6b8aeeeb86d3cdad7897a9fe817393161ac
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-06-03 01:35:22 +0900

    동시접속 고침

--- Server/Server/Server/GameServer/GameServer.cpp
+++ Server/Server/Server/GameServer/GameServer.cpp
@@ -388,7 +388,7 @@ void process_packet(int s_id, char* p)
 		}
 
 		// 새로 접속한 플레이어에게 주위 객체 정보를 보낸다
-		for (auto& other : clients) {
+		/*for (auto& other : clients) {
 			if (other._s_id == cl._s_id) continue;
 			other.state_lock.lock();
 			if (ST_INGAME != other._state) {
@@ -413,7 +413,7 @@ void process_packet(int s_id, char* p)
 			printf_s("[어떤 클라의 Send put object] id : %d, location : (%f,%f,%f), yaw : %f\n", packet.id, packet.x, packet.y, packet.z, packet.yaw);
 
 			cl.do_send(sizeof(packet), &packet);
-		}
+		}*/
 		
 		break;
 	}
