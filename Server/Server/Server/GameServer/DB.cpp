f9bbda2e9d2e7527efbc9761f265df0a52d75978
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-06-10 19:50:01 +0900

    db 저장된 데이터 불러오는 프로시저 및 클래스 생성

--- Server/Server/Server/GameServer/DB.cpp
+++ Server/Server/Server/GameServer/DB.cpp
@@ -3,6 +3,9 @@
 #include <sqlext.h>
 #include <iostream>
 #include <string>
+#define NAME_LEN 20 
+
+
 void show_err() {
 	cout << "error" << endl;
 }
@@ -75,4 +78,83 @@ void save_data() {
         }
         SQLFreeHandle(SQL_HANDLE_ENV, henv);
     }
+}
+
+
+bool DB_odbc(int id, char* name, char* pw)
+{
+	SQLHENV henv;
+	SQLHDBC hdbc;
+	SQLHSTMT hstmt = 0;
+	SQLRETURN retcode;
+
+	char temp[BUFSIZE] = {};
+	sprintf_s(temp, sizeof(temp), "EXEC [GraduationWork].[dbo].login_player %s,%s", name, pw);
+	wchar_t* exec;
+	int str_size = MultiByteToWideChar(CP_ACP, 0, temp, -1, NULL, NULL);
+	exec = new WCHAR[str_size];
+	MultiByteToWideChar(CP_ACP, 0, temp, sizeof(temp) + 1, exec, str_size);
+
+	SQLWCHAR p_id[NAME_LEN] = {};
+	SQLLEN cbP_ID = 0;
+
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
+
+	// Set the ODBC version environment attribute  
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);
+
+
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
+
+
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
+
+
+				retcode = SQLConnect(hdbc, (SQLWCHAR*)L"2024GraduationWork", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+
+
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					cout << "ODBC Connection Success" << endl;
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+					retcode = SQLExecDirect(hstmt, (SQLWCHAR*)exec, SQL_NTS);
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						retcode = SQLBindCol(hstmt, 1, SQL_C_WCHAR, p_id, NAME_LEN, &cbP_ID);
+
+						for (int i = 0; ; i++) {
+							retcode = SQLFetch(hstmt);
+							if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+								show_err();
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+							{
+								if (p_id != 0) {
+									return true;
+								}
+								else
+								{
+									cout << "login Failed" << endl;
+									return false;
+								}
+							}
+							else
+								break;
+						}
+					}
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						SQLCancel(hstmt);
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+					}
+
+					SQLDisconnect(hdbc);
+				}
+				else cout << "ODBC Connected Failed" << endl;
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+
+	return false;
 }
