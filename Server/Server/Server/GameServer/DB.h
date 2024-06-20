a9d5f5313704188841e7096b59f8d694438c60d5
KwonSe-Jin <112988318+KwonSe-Jin@users.noreply.github.com>
2024-06-20 21:55:24 +0900

    중복접속 체크

--- Server/Server/Server/GameServer/DB.h
+++ Server/Server/Server/GameServer/DB.h
@@ -5,6 +5,6 @@
 #include <string>
 
 void show_err();
-
+void printSQLError(SQLHANDLE handle, SQLSMALLINT type);
 void save_data(const char* id, const char* pw);
 bool DB_odbc(const char* name, const char* pw);
