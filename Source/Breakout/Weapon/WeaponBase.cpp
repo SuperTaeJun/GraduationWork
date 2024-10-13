af4707324ef9e4890b4416dbaa5bfe720c19aedf
SuperTaeJun <jtj990222@naver.com>
2024-10-14 00:59:52 +0900

    주석

--- Source/Breakout/Weapon/WeaponBase.cpp
+++ Source/Breakout/Weapon/WeaponBase.cpp
@@ -50,12 +50,17 @@ void AWeaponBase::BeginPlay()
 
 FVector AWeaponBase::TraceEndWithScatter(const FVector& TraceStart, const FVector& HitTarget)
 {
-	//UE_LOG(LogTemp, Log, TEXT("TRACE"));
-	FVector ToTargetNormalized = (HitTarget - TraceStart).GetSafeNormal();
-	FVector SphereCenter = TraceStart + ToTargetNormalized * DistanceToSphere;
+
+	// 목표 위치와 시작 위치의 방향을 정규화
+	FVector DirectionToTarget = (HitTarget - TraceStart).GetSafeNormal();
+	// 시작 위치에서 목표 방향으로 DistanceToSphere만큼 이동한 지점을 구의 중심으로 설정합니다.
+	FVector SphereCenter = TraceStart + DirectionToTarget * DistanceToSphere;
+	// 구의 내부에서 난수로 생성된 벡터를 계산하여 랜덤 위치를 만듭니다.
 	FVector RandVector = UKismetMathLibrary::RandomUnitVector() * FMath::FRandRange(0.f, SphereRadius);
+	// 구의 중심에 난수 벡터를 추가하여 최종 위치를 계산합니다.
 	FVector EndLoc = SphereCenter + RandVector;
-	FVector ToEndLoc = EndLoc - TraceStart;
+	// 시작 위치에서 최종 위치까지의 벡터를 계산합니다.
+	FVector DirToEndLoc = EndLoc - TraceStart;
 
 	if (bDebug)
 	{
@@ -64,12 +69,12 @@ FVector AWeaponBase::TraceEndWithScatter(const FVector& TraceStart, const FVecto
 		DrawDebugLine(
 			GetWorld(),
 			TraceStart,
-			FVector(TraceStart + ToEndLoc * 1000.f / ToEndLoc.Size()),
+			FVector(TraceStart + DirToEndLoc * 1000.f / DirToEndLoc.Size()),
 			FColor::Cyan,
 			false,
 			0.5f);
 	}
-	return FVector(TraceStart + ToEndLoc * Range / ToEndLoc.Size());
+	return FVector(TraceStart + DirToEndLoc);
 }
 
 void AWeaponBase::WeaponTraceHit(const FVector& TraceStart, const FVector& HitTarget, FHitResult& OutHit)
